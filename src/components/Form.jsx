922eaa521c501da6ce00dd5f1573f1e057ea9ca1
mauroblanco7 <mauro.blnc.mb@gmail.com>
2022-08-30 17:38:18 -0300

    validaciones del formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,104 @@
-import React from 'react'
+// import React from 'react'
+// import emailjs from 'email-js';
 
-const Form = () => {
+// const Form = () => {
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm('gmail', 'template_hdluw1p', form.current, 'KGIOnLSnFM37YOMNq')
+//       .then((result) => {
+//           console.log(result.text);
+//       }, (error) => {
+//           console.log(error.text);
+//       });
+//       form.current.reset()
+//   };
+//   return (
+//     <form onSubmit={sendEmail}>
+
+  // <div class="form-outline mb-4">
+  //   <input type="text" id="form4Example1" name='name' class="form-control" />
+  //   <label class="form-label" for="form4Example1">Name</label>
+  // </div>
+
+
+  // <div class="form-outline mb-4">
+  //   <input type="email" id="form4Example2" name='email' class="form-control" />
+  //   <label class="form-label" for="form4Example2">Email</label>
+  // </div>
+
+
+  // <div class="form-outline mb-4">
+  //   <textarea class="form-control" name='message' id="form4Example3" rows="4"></textarea>
+  //   <label class="form-label" for="form4Example3">Mensaje</label>
+  // </div>
+
+
+//   <button type="submit" class=" btn-block mb-4">Enviar</button>
+// </form>
+//   )
+// }
+
+// export default Form
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
+
+export const Form = () => {
+  let regexName = /[a-z]{4,10}$/;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+  if(form.current.name.value.length
+     && form.current.surname.value.length
+     && form.current.email.value && 
+     form.current.message.value){
+      if(regexName.test(form.current.name.value) && regexName.test(form.current.surname.value) ){
+        emailjs.sendForm('gmail', 'template_hdluw1p', form.current, 'KGIOnLSnFM37YOMNq')
+        .then((result) => {
+            console.log(result.text);
+            Swal.fire(`${form.current.name.value}`,"Tu email fue enviado con exito","success")
+            form.current.reset()
+        }, (error) => {
+            console.log(error.text);
+            Swal.fire(`${error.text}`,"Tu email no fue enviado por motivos desconocidos.Puedes escribirme a mi email mauro.blnc.mb@gmail.com. Gracias!","error")
+        });
+        form.current.reset()
+      } else {
+        Swal.fire(``,"El nombre y apellido debe contener de 3 a 10 caracteres","error")
+      }
+      
+   } else{
+    Swal.fire(``,"Todos los campos son requeridos","error")
+   }
+
+  };
+
   return (
-    <form>
-
+    <form ref={form} onSubmit={sendEmail}>
+      
   <div class="form-outline mb-4">
-    <input type="text" id="form4Example1" class="form-control" />
-    <label class="form-label" for="form4Example1">Name</label>
+    <input type="text" id="form4Example1" name='name' class="form-control" />
+    <label class="form-label" for="form4Example1">Nombre</label>
   </div>
-
-
   <div class="form-outline mb-4">
-    <input type="email" id="form4Example2" class="form-control" />
+    <input type="text" id="form4Example1" name='surname' class="form-control" />
+    <label class="form-label" for="form4Example1">Apellido</label>
+  </div>
+  <div class="form-outline mb-4">
+    <input type="email" id="form4Example2" name='email' class="form-control" />
     <label class="form-label" for="form4Example2">Email</label>
   </div>
-
-
+  
   <div class="form-outline mb-4">
-    <textarea class="form-control" id="form4Example3" rows="4"></textarea>
+    <textarea class="form-control" name='message' maxlength="400" id="form4Example3" rows="4"></textarea>
     <label class="form-label" for="form4Example3">Mensaje</label>
   </div>
-
-
-  <button type="submit" class=" btn-block mb-4">Enviar</button>
-</form>
-  )
-}
-
+  <button type="submit" value="Send" class=" btn-block mb-4">Enviar</button>
+     
+    </form>
+  );
+};
 export default Form
